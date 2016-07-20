7a9bac176d7fabbea454851bec610eaea7be5890
Trent Johnson <tdjohnson0722@gmail.com>
2016-07-20 14:15:41 -0500

    there we go

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express=require('express');
+var index=require('./routes/index');
+var people=require('./routes/people');
 var app=express();
+
+//static files
+app.use(express.static('public'));
+//routes
+app.use('/', index);
+app.use('/people', people)
 
 var server=app.listen(3000, function(){
   var port=server.address().port;
