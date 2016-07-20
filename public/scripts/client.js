783f46195e2fb4a2e3fe9dd3e10598d7bbf9abc4
Trent Johnson <tdjohnson0722@gmail.com>
2016-07-20 15:15:24 -0500

    get calls

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,12 +2,17 @@ var app = angular.module('patronApp', []);
 
 app.controller('MainController', function($scope, $http){
   $scope.showError = false;
-
-  function handleSuccess(response) {
-    $scope. = response.data;
+  $http.get('/people').then(handlePeopleSuccess, handleFailure);
+  $http.get('/patronuses').then(handlePatronSuccess, handleFailure);
+  function handlePeopleSuccess(response) {
+    $scope.people = response.data;
     console.log('Success', response);
   }
-  
+  function handlePatronSuccess(response) {
+    $scope.patronuses = response.data;
+    console.log('Success', response);
+  }
+
   function handleFailure(response) {
     console.log('Failure', response);
   }
