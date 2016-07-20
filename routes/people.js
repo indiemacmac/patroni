30d91a375638539237163f6ce1b3b5b94f2215af
Trent Johnson <tdjohnson0722@gmail.com>
2016-07-20 11:46:41 -0500

    added route people.js

---
+++ routes/people.js
@@ -0,0 +1,16 @@
+var router=require('express').Router();
+
+router.get('/', function(request,response){
+
+});
+router.get('/personid', function(request, response){
+
+});
+router.post('/', function(requeset, response){
+
+});
+router.put('/personid', function(request, response){
+
+});
+
+module.exports=router;
